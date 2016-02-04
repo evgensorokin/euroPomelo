cdad797baca43d5c3413b5d39ed6f6df8f9fb79e
Evgen <bloodelf1985@gmail.com>
2016-02-04 19:37:49 +0200

    add files

--- wp-content/themes/euroPomelo/page-recipes.php
+++ wp-content/themes/euroPomelo/page-recipes.php
@@ -17,35 +17,56 @@ get_header('pomelo'); ?>
     <div class="container">
 
         <h2 class="text-center">Recepies We Recommand</h2>
+        <?php
+            $paged = ( get_query_var('paged') > 1 ) ? get_query_var('paged') : 1;
 
-        <div class="row clearfix">
-            <?php
-                $args = array('post_type' => 'recipe', 'posts_per_page' => 4, 'orderby' => 'post_date', 'order' => 'ASC');
-                $loop = new WP_Query( $args );
+            $args = array('post_type' => 'recipe', 'posts_per_page' => 3, 'orderby' => 'post_date', 'order' => 'ASC', 'paged' => $paged);
+            $loop = new WP_Query( $args );
 
-                if( $loop->have_posts() ) :
-                    while ( $loop->have_posts() ) : $loop->the_post();
-                    $feat_image = wp_get_attachment_url( get_post_thumbnail_id(get_the_ID()) );
-            ?>
-                    <div class="col-md-4 col-sm-6 recepie">
-                        <div class="img" style="background: url('<?php echo $feat_image; ?>') center no-repeat;"></div>
-                        <div class="color-box" style="background-color: <?php the_field('background_color_item'); ?>;">
-                            <div>
-                                <div class="title text-center"><?php the_title(); ?></div>
-                                <hr/>
-                                <ul>
-                                    <li><span>1.</span> Mozzarella di Bufala  300g</li>
-                                    <li><span>2.</span> Tomato 200g</li>
-                                    <li><span>3.</span> Mozzarella di Bufala 100g</li>
-                                </ul>
-                                <div class="text-center"><a href="<?php the_permalink(); ?>">View Recipe</a></div>
+            $max = $loop->max_num_pages;
+
+            // Add some parameters for the JS.
+            wp_localize_script(
+                'pbd-alp-load-posts',
+                'pbd_alp',
+                array(
+                    'startPage' => $paged,
+                    'maxPages' => $max,
+                    'nextLink' => next_posts($max, false)
+                )
+            );
+
+            if( $loop->have_posts() ) :
+        ?>
+            <div class="row clearfix">
+                <?php while ( $loop->have_posts() ) : $loop->the_post();
+                        $feat_image = wp_get_attachment_url( get_post_thumbnail_id(get_the_ID()) );
+                    ?>
+                        <div class="col-md-4 col-sm-6 recepie post">
+                            <div class="img" style="background: url('<?php echo $feat_image; ?>') center no-repeat;"></div>
+                            <div class="color-box" style="background-color: <?php the_field('background_color_item'); ?>;">
+                                <div>
+                                    <div class="title text-center"><?php the_title(); ?></div>
+                                    <hr/>
+                                    <ul>
+                                        <?php $posts = get_field('select_products'); if( $posts ) : ?>
+                                            <?php foreach( $posts as $post) : setup_postdata($post); ?>
+                                            <li><span>1.</span> <?php the_title(); ?></li>
+                                            <?php endforeach; ?>
+                                            <?php wp_reset_postdata(); ?>
+                                        <?php endif; ?>
+                                        <li><span>2.</span> Tomato 200g</li>
+                                        <li><span>3.</span> Mozzarella di Bufala 100g</li>
+                                    </ul>
+                                    <div class="text-center"><a href="<?php the_permalink(); ?>">View Recipe</a></div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-            <?php endwhile; endif; ?>
-        </div>
+                <?php endwhile; ?>
+            </div>
 
-        <div id="content" role="main"></div>
+            <div id="content" role="main"></div>
+        <?php endif; ?>
 
     </div>
 </section>
